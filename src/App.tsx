e6021def7fb0ea37b317109b3ca1da343ec05bf5
Arihant Jain <arihant.jain67@gmail.com>
2023-01-26 19:56:13 +0530

    does it work?

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const obj = {
+    a: 1,
+    b: 2,
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> {obj.a} and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener  oreferrer noreferrer"
         >
           Learn React
         </a>
